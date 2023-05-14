d046f9a2db66664e7283cdf70dea07640afa5d01
Menelik Tefera <menelik.tefera@ssf.no>
2023-05-14 17:01:39 +0200

    Adding mutation and updating dependencies and syntax with latest from apollo client

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,12 +1,23 @@
 import React, { useState } from 'react';
+import { gql, useMutation } from '@apollo/client';
 
 export default function SongCreate() {
   const [title, setTitle] = useState('');
+  const [createSong, { data, loading, error }] = useMutation(CREATE_SONG);
+
+  if (loading) return 'Submitting...';
+  if (error) return `Submission error! ${error.message}`;
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    createSong({ variables: { title } });
+    setTitle('');
+  };
 
   return (
     <div>
       <div>Create a New Song</div>
-      <form>
+      <form onSubmit={onSubmit}>
         <label>Song title</label>
         <input
           onChange={(event) => setTitle(event.target.value)}
@@ -16,3 +27,11 @@ export default function SongCreate() {
     </div>
   );
 }
+
+const CREATE_SONG = gql`
+  mutation CreateSong($title: String) {
+    addSong(title: $title) {
+      title
+    }
+  }
+`;
